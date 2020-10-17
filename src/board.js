01e0b91efe43d922d7575234c0178703ad5e5f3e
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2020-10-17 13:40:33 +0530

    clear action added + css changes

--- src/board.js
+++ src/board.js
@@ -8,30 +8,66 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares : Array(9).fill(null),
-            xIsNext : true
+            xIsNext : true,
+            completed: false
         };
     }
 
     handleClick(i) {
         let squares = this.state.squares.slice();
-        if (this.calculateWinner(squares)) return;
+        if (this.hasEntry(squares, i)) return;
+
         squares[i] = this.state.xIsNext ? "X" : "0";
+        let completed = false;
+
+        if (this.calculateWinnerPosition(squares) || this.checkAllFilled(squares)) {
+            completed = true;
+        }
         this.setState(
             {squares: squares,
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            completed: completed
         });
     }
 
+    startGame() {        
+        this.setState(
+            {squares: Array(9).fill(null),
+            xIsNext: true,
+            completed: false
+        });
+    }
+
+
+    checkAllFilled(sqaures){
+        return !sqaures.includes(null);
+    }
+    
+    hasEntry(sqaures, i) {
+        return sqaures[i]; 
+    }
+
     renderSquare(i) {
+        const winnerPosition = this.calculateWinnerPosition(this.state.squares);
+        let winner = false;
+        if (winnerPosition && winnerPosition.includes(i)) {
+            winner = true;
+        }
         return (
             <Square 
               value={this.state.squares[i]}
+              completed={this.state.completed}
+              winner={winner}
               onClick={() => this.handleClick(i)} 
             />
         )
     }
+
+    getWinner(squares, winnerPosition) {
+        return squares[winnerPosition[0]];
+    }
     
-    calculateWinner(squares) {
+    calculateWinnerPosition(squares) {
         const lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -45,26 +81,41 @@ class Board extends React.Component {
         for (let i=0; i<lines.length; i++) {
             const [a, b, c] = lines[i];
             if (squares[a] != null && squares[b] != null && squares[c] != null) {
-                if (squares[a] === squares[b] === squares[c]) {
-                    return squares[a];
+                if (squares[a] === squares[b] && squares[a] === squares[c]) {
+                    return [a, b, c];
                 }
             }
         }
         return null;
     }
-;
+
+    renderClearButton() {
+        if (this.state.completed) {
+            return (
+                <button className="refreshBtn"
+                onClick={() => this.startGame()}>clear</button>
+            )
+        }
+    }
+
     render() {
-        const winner = this.calculateWinner(this.state.squares);
+        const winnerPosition = this.calculateWinnerPosition(this.state.squares);
+        let winner = null;
+        if (winnerPosition) {
+            winner = this.getWinner(this.state.squares, winnerPosition);
+        }
         let status;
         if (winner) {
-            status = "Winner : " + winner;
+            status = winner + " won the game!";
+        } else if (this.state.completed){
+            status = "Game Over";
         } else {
-            status = 'Next Player : ' + (this.state.xIsNext ? 'X' : '0');
+            status = "Next player is " + (this.state.xIsNext ? 'X' : '0');
         }
-
         return (
             <div className="gameBoard">
-                <div className="status">{status}</div>
+                <div className="gameTitle">Tic Tac Toe
+                </div>
                 <div className="row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -80,10 +131,12 @@ class Board extends React.Component {
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
                 </div>
+                <div className="status">{status}</div>
+                {this.renderClearButton()}
             </div>
         );
     }
+    
 }
 
 export default Board;
-
--- src/square.js
+++ src/square.js
@@ -3,9 +3,14 @@ import React from "react";
 import "./square.css"
 
 function Square(props) {
+    let classname = "square";
+    if (props.winner) {
+        classname = "winnerSquare";
+    }
     return (
-        <button className="square" 
-            onClick={() => props.onClick()}>
+        <button className={classname}
+            onClick={() => props.onClick()}
+            disabled={props.completed}>
             {props.value}
         </button>
     )
